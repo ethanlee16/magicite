ef4449d830c2d1559db4e0cb4827d0ec699b52b8
Ethan Lee <ethanlee@ethansites.com>
2015-08-09 01:26:19 -0700

    API keys now modular and private

--- popup.js
+++ popup.js
@@ -1,47 +1,62 @@
 function renderStatus(text){
-	document.getElementById('status').textContent = text;
+    document.getElementById('status').textContent = text;
 }
 
 function updateStatus(title, url) {
-	var listItem = document.createElement("p");
-	var titleItem = document.createElement("span")
-	titleItem.className = "tab-title";
-	titleItem.textContent = title;
-	listItem.appendChild(titleItem);
-	listItem.innerHTML +=  " - " + url;
-	document.getElementById('status').appendChild(listItem);
+    var listItem = document.createElement("p");
+    var titleItem = document.createElement("span")
+    titleItem.className = "tab-title";
+    titleItem.textContent = title + " - ";
+    listItem.appendChild(titleItem);
+    listItem.innerHTML +=  url;
+    document.getElementById('status').appendChild(listItem);
 }
 
 function createRequest(url) {
-	//create JSON for ONE url here
+    return {
+        "key": keys.easybib,
+        "source": "website",
+        "website": {
+            "title": ""
+        },
+        "pubonline": {
+            "title": "",
+            "day": 1,
+            "month": 1,
+            "year": 1969,
+            "url": "",
+            "dayaccessed": Date.getDay(),
+            "monthaccessed": Date.getMonth(),
+            "yearaccessed": Date.getFullYear()
+        }
+    }
 }
 function cite(urlArray) {
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", "http://www.easybib.com/cite/bulk", true);
-		xhr.onreadystatechange = function() {
-  			if (xhr.readyState == 4) {
-    			var res = JSON.parse(xhr.responseText);
-                console.log(res);
-  			}
-		}
-		var request;
-		for (var i = 0; i < urlArray.length; i++) {
-			request.push(createRequest(urlArray[i]));
-		}
-	xhr.send(request);
-	
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://www.easybib.com/cite/bulk", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            var res = JSON.parse(xhr.responseText);
+            console.log(res);
+        }
+    }
+    var request = [];
+    for (var i = 0; i < urlArray.length; i++) {
+        request.push(createRequest(urlArray[i]));
+    }
+    xhr.send(request);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	chrome.tabs.query({}, function (tabs) {
-		for(var i = 0; i < tabs.length; i++) {
-			var url = "";
-			if(tabs[i].url.length > 36) {
-				url = tabs[i].url.substring(0, 36) + "...";
-			} else {
-				url = tabs[i].url;
-			}
-			updateStatus(tabs[i].title, url);
-		}
-	});
+    chrome.tabs.query({}, function (tabs) {
+        for(var i = 0; i < tabs.length; i++) {
+            var url = "";
+            if(tabs[i].url.length > 36) {
+                url = tabs[i].url.substring(0, 36) + "...";
+            } else {
+                url = tabs[i].url;
+            }
+            updateStatus(tabs[i].title, url);
+        }
+    });
 });
