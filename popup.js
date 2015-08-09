896aaeec199a36e504dcf3ec256f98c80855a7bc
Ethan Lee <ethanlee@ethansites.com>
2015-08-08 23:45:02 -0700

    Displays active tab's URL

--- popup.js
+++ popup.js
@@ -2,32 +2,14 @@ function renderStatus(text){
 	document.getElementById('status').textContent = text;
 }
 
+function updateStatus(text) {
+	document.getElementById('status').textContent += text;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-	renderStatus("Ethan's alive!");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-})
+	chrome.tabs.query({active: true}, function (tabs) {
+		for(var i = 0; i < tabs.length; i++) {
+			updateStatus(tabs[i].url);
+		}
+	});
+});
