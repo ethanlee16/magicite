e235e528dd63242b2718d559555ae64b2f1c6a89
kushal tirumala <kushaltirumala99@gmail.com>
2015-08-09 00:58:48 -0700

    commented out EasyBib Api integration

--- popup.js
+++ popup.js
@@ -8,6 +8,26 @@ function updateStatus(text) {
 	document.getElementById('status').appendChild(listItem);
 }
 
+function createRequest(url) {
+	//create JSON for ONE url here
+}
+function cite(urlArray) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "http://www.easybib.com/cite/bulk", true);
+		xhr.onreadystatechange = function() {
+  			if (xhr.readyState == 4) {
+    			var res = JSON.parse(xhr.responseText);
+                console.log(res);
+  			}
+		}
+		var request;
+		for (var i = 0; i < urlArray.length; i++) {
+			request.push(createRequest(urlArray[i]));
+		}
+	xhr.send(request);
+	
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	chrome.tabs.query({}, function (tabs) {
 		for(var i = 0; i < tabs.length; i++) {
